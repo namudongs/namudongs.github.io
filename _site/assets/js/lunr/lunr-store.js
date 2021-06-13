63db72d349dbcd0badd3ddd9ce07a2ccd2abdf93
namdong <namdong@namdongui-MacBookAir.local>
2021-06-13 17:57:14 +0900

    update 0.14

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,12 @@ var store = [{
         "title": "RC Chapter 1. 문장 형식",
         "excerpt":"토익에 나오는 문장 형식은 총 5가지가 있다.  1형식 S V  2형식 S V S.C  3형식 S V O  4형식 S V SB STH  5형식 S V O.C   ","categories": ["rc"],
         "tags": ["toeic","study"],
-        "url": "/rc/first-post/",
+        "url": "/rc/shape/",
+        "teaser": null
+      },{
+        "title": "RC Chapter 2. 동명사",
+        "excerpt":"토익에 나오는 동명사는 다음과 같다.     동명사   현재분사형용사  ","categories": ["rc"],
+        "tags": ["toeic","study"],
+        "url": "/rc/ving/",
         "teaser": null
       }]
